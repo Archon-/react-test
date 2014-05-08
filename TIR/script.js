9269dcc24c19b33d29344571b8a87087b4798089
Szymon Konczal <archon@archons.info>
2014-05-08 14:19:45 +0200

    Added some features

--- TIR/script.js
+++ TIR/script.js
@@ -54,14 +54,15 @@ var ProductTable = React.createClass({
         var rows = [];
         var lastCategory = null;
         this.props.products.forEach(function(product) {
+            if(product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)){
+                return;
+            }
             if (product.category !== lastCategory) {
                 rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
             }
             rows.push(<ProductRow product={product} key={product.name} />);
-            console.log("product: ");
-            console.log(product.category);
             lastCategory = product.category;
-        });
+        }.bind(this));
         return (
             <table>
                 <thead>
@@ -86,9 +87,9 @@ var SearchBar = React.createClass({
     render: function() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="Search..." />
+                <input type="text" placeholder="Search..." value={this.props.filterText} />
                 <p>
-                    <input type="checkbox" />
+                    <input type="checkbox" value={this.props.inStockOnly}/>
                     Only show products in stock
                 </p>
             </form>
@@ -103,11 +104,25 @@ var SearchBar = React.createClass({
  */
 
 var FilterableProductTable = React.createClass({
+    getInitialState: function(){
+        return {
+            filterText: '',
+            inStockOnly: false
+        }
+    },
+
     render: function() {
         return (
             <div>
-                <SearchBar />
-                <ProductTable products={this.props.products} />
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
+                <ProductTable 
+                    products={this.props.products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
             </div>
         );
     }
